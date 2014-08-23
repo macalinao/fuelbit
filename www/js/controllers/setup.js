83c56f68dafb6d82b6d599bb0eb1add5db51c69c
Ian Macalinao <ian@giza.us>
2014-08-22 22:29:27 -0500

    Fix sync button

--- www/js/controllers/setup.js
+++ www/js/controllers/setup.js
@@ -1,6 +1,8 @@
 angular.module('fuelbit')
   .controller('SetupCtrl', function($scope, $location, vehicle) {
+    $scope.cfg = {};
     $scope.submit = function() {
+      vehicle.cfg = $scope.cfg;
       $location.path('/');
     };
   });
