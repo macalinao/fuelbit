4b3a83d62157b2edf83327f9fb434f2a645f3b40
Ian Macalinao <ian@giza.us>
2014-08-23 17:01:26 -0500

    Fix graphs etc

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -18,6 +18,19 @@ angular.module('fuelbit')
     };
     $scope.onHomeTab();
 
+    $scope.fieldClasses = {
+      one: {
+        gas: '',
+        dist: '',
+        time: ''
+      },
+      two: {
+        gas: '',
+        dist: '',
+        time: ''
+      }
+    };
+
     $scope.compareDate = {};
     $scope.setDate = function(d) {
       var ret = _.find(Data.days, function(day) {
@@ -25,21 +38,65 @@ angular.module('fuelbit')
       });
       if (ret) {
         $scope.compareDays[d] = ret;
+        if ($scope.compareDays.one && $scope.compareDays.two) {
+          $scope.compareGraph();
+          // compare stuff and save winner/losers
+          var one = $scope.compareDays.one;
+          var two = $scope.compareDays.two;
+          if (one.gas < two.gas) {
+            $scope.fieldClasses.one.gas = 'winner';
+            $scope.fieldClasses.two.gas = 'loser';
+          } else {
+            $scope.fieldClasses.one.gas = 'loser';
+            $scope.fieldClasses.two.gas = 'winner';
+          }
+          if (one.dist < two.dist) {
+            $scope.fieldClasses.one.dist = 'winner';
+            $scope.fieldClasses.two.dist = 'loser';
+          } else {
+            $scope.fieldClasses.one.dist = 'loser';
+            $scope.fieldClasses.two.dist = 'winner';
+          }
+          if (one.time < two.time) {
+            $scope.fieldClasses.one.time = 'winner';
+            $scope.fieldClasses.two.time = 'loser';
+          } else {
+            $scope.fieldClasses.one.time = 'loser';
+            $scope.fieldClasses.two.time = 'winner';
+          }
+        }
       }
     };
 
     $scope.compareDays = {};
     $scope.compareGraph = function() {
+      var one = $scope.compareDays.one;
+      var two = $scope.compareDays.two;
+
       $('#compareGraph').kendoChart({
-        title: 'Comparison of days...'
+        title: 'Day Comparison',
+        legend: {
+          visible: false
+        },
+        seriesDefaults: {
+          type: 'bar'
+        },
+        series: [{
+          name: 'Day One',
+          data: [one.gas, one.dist, one.time]
+        }, {
+          name: 'Day Two',
+          data: [two.gas, two.dist, two.time]
+        }],
+        categoryAxis: [{
+          categories: ['Gas', 'Distance', 'Time']
+        }],
+        tooltip: {
+          visible: true,
+          template: "#= series.name #: #= value #"
+        }
       });
     };
 
     $scope.data = {};
-
-    $scope.gasUsage = function() {
-      setTimeout(function() {
-        $('#gasUsage').kendoChart();
-      }, 0);
-    };
   });
