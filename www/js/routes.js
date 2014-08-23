e845ef1c3fbbfefc1de8d1e6fea9d7f411817b30
Ian Macalinao <ian@giza.us>
2014-08-22 21:59:27 -0500

    Add setup route

--- www/js/routes.js
+++ www/js/routes.js
@@ -5,6 +5,11 @@ angular.module('energypal')
         url: '/',
         templateUrl: 'templates/main.html',
         controller: 'MainCtrl'
+      })
+      .state('setup', {
+        url: '/setup',
+        templateUrl: 'templates/setup.html',
+        controller: 'SetupCtrl'
       });
 
     $urlRouterProvider.otherwise('/');
