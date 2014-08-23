95cfca034f123d27c881d3f14a35c4d56a4ab76f
Ian Macalinao <ian@giza.us>
2014-08-23 07:38:23 -0500

    Attempt #1 of getting graphs working

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -2,7 +2,10 @@ angular.module('fuelbit')
   .controller('MainCtrl', function($scope, $location, vehicle) {
     if (!vehicle.data) {
       vehicle.initData();
-      //$location.path('/setup');
+      // $location.path('/setup');
+      // return;
     }
     $scope.vehicle = vehicle.data.vehicle;
+
+    $('#gasUsage').kendoChart();
   });
