dd897559079757af498320c7530f2da8d6491c67
Ian Macalinao <ian@giza.us>
2014-08-23 00:00:36 -0500

    Add details of car stuff

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,6 +1,8 @@
 angular.module('fuelbit')
   .controller('MainCtrl', function($scope, $location, vehicle) {
     if (!vehicle.data) {
-      $location.path('/setup');
+      vehicle.initData();
+      //$location.path('/setup');
     }
+    $scope.vehicle = vehicle.data.vehicle;
   });
